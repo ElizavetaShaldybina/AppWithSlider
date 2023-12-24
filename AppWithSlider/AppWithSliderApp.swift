1dcbd400423e9cdb2638553370b88d7483e2afba
Elizaveta <elizavetash0210@mail.ru>
2023-12-24 09:33:08 +0300

    Initial Commit

---
+++ AppWithSlider/AppWithSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppWithSliderApp.swift
+//  AppWithSlider
+//
+//  Created by Елизавета Шалдыбина on 24.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppWithSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
